f0b5f3dde8d2024028c124e53edc650b259d8c5c
DiabateJS <diabatejs@yahoo.fr>
2023-10-08 19:00:08 +0200

    Initial commit

---
+++ src/test/java/fr/djstechno/sensibilisationclimatspringboot/SensibilisationClimatSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.djstechno.sensibilisationclimatspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensibilisationClimatSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
