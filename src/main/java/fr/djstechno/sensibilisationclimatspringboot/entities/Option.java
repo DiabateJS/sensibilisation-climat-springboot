b105ff82aca94dbfe7b93bc413adf79e3362302e
ET34471 <jean.diabate@bred.fr>
2023-10-17 15:12:56 +0200

    Amélioration du model Option et des requetes SQL.

--- src/main/java/fr/djstechno/sensibilisationclimatspringboot/entities/Option.java
+++ src/main/java/fr/djstechno/sensibilisationclimatspringboot/entities/Option.java
@@ -1,16 +1,21 @@
 package fr.djstechno.sensibilisationclimatspringboot.entities;
 
-import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-import javax.persistence.*;
+import lombok.Data;
 
 @Data
 @Entity
-@Table(name="ASC_OPTION")
+@Table(name = "ASC_OPTION")
 public class Option {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String idOption;
     private String valeur;
     private Long idQuestion;
 
--- src/main/resources/quiz_h2.sql
+++ src/main/resources/quiz_h2.sql
@@ -4,33 +4,33 @@ insert into ASC_QUIZ values (2, 'QZ002','Quiz Niveau Intermédiaire','Un jour pa
 -- Quiz 1
 insert into ASC_QUESTION values (1, 'Heureusement, le phénomène naturel de l effet de serre conserve dans l atmosphère une partie de la chaleur du soleil !', 1, 'A' , 'Quelle serait la température de la Terre sans atmosphère ?');
 -- Options de la question
-insert into ASC_OPTION values (1, 'A','+15 °C',1);
-insert into ASC_OPTION values (2, 'B','0 °C',1);
-insert into ASC_OPTION values (3, 'C','-50 °C',1);
-insert into ASC_OPTION values (4, 'D','-18 °C',1);
+insert into ASC_OPTION values (1, 'A',1,'+15 °C');
+insert into ASC_OPTION values (2, 'B',1,'0 °C');
+insert into ASC_OPTION values (3, 'C',1,'-50 °C');
+insert into ASC_OPTION values (4, 'D',1,'-18 °C');
 
 insert into ASC_QUESTION values (2,'La vapeur d eau contribue pour 60 % à l effet de serre. Les émissions de ce gaz par l humanité à travers l industrie n ont cependant qu un effet limité car la durée de vie de la vapeur d eau est courte.',1,'B', 'Quel gaz a l effet de serre le plus puissant ?');
 -- Options de la question
-insert into ASC_OPTION values (5, 'A','CO2',2);
-insert into ASC_OPTION values (6, 'B','Vapeur d eau',2);
-insert into ASC_OPTION values (7, 'C','Méthane',2);
-insert into ASC_OPTION values (8, 'D','Argon',2);
+insert into ASC_OPTION values (5, 'A',2,'CO2');
+insert into ASC_OPTION values (6, 'B',2,'Vapeur d eau');
+insert into ASC_OPTION values (7, 'C',2,'Méthane');
+insert into ASC_OPTION values (8, 'D',2,'Argon');
 
 insert into ASC_QUESTION values (3, 'Plus de 50% est encore absorbé par l atmosphère.', 1,'A','Où va la majorité du CO2 émis par l Homme ?');
 -- Options de la question
-insert into ASC_OPTION values (9, 'A','Dans l atmosphère',3);
-insert into ASC_OPTION values (10, 'B','Dans l océan',3);
-insert into ASC_OPTION values (11, 'C','Méthane',3);
+insert into ASC_OPTION values (9, 'A',3,'Dans l atmosphère');
+insert into ASC_OPTION values (10, 'B',3,'Dans l océan');
+insert into ASC_OPTION values (11, 'C',3,'Méthane');
 
 insert into ASC_QUESTION values (4, 'À quantité égale, produire une tomate en hiver génère aujourd hui plus de C02 que le transport des autres aliments...', 1,'B','En hiver et à quantité égale, lequel de ces aliments émet le plus de CO2 ?');
 -- Options de la question
-insert into ASC_OPTION values (12, 'A','Banane de Martinique',4);
-insert into ASC_OPTION values (13, 'B','Tomates d Espagne',4);
-insert into ASC_OPTION values (14, 'C','Le rutabaga picard',4);
-insert into ASC_OPTION values (15, 'D','Le café colombien',4);
+insert into ASC_OPTION values (12, 'A',4,'Banane de Martinique');
+insert into ASC_OPTION values (13, 'B',4,'Tomates d Espagne');
+insert into ASC_OPTION values (14, 'C',4,'Le rutabaga picard');
+insert into ASC_OPTION values (15, 'D',4,'Le café colombien');
 
 -- Quiz 2
-insert into ASC_QUESTION values (5, 'Le GEIC a bien été crée en 1988', 1 ,'A','En quelle année le GEIC a été crée ?');
-insert into ASC_OPTION values (16, 'A','1988',5);
-insert into ASC_OPTION values (17, 'B','1999',5);
-insert into ASC_OPTION values (18, 'C','2005',5);
+insert into ASC_QUESTION values (5, 'Le GEIC a bien été crée en 1988', 2 ,'A','En quelle année le GEIC a été crée ?');
+insert into ASC_OPTION values (16, 'A',5,'1988');
+insert into ASC_OPTION values (17, 'B',5,'1999');
+insert into ASC_OPTION values (18, 'C',5,'2005');
